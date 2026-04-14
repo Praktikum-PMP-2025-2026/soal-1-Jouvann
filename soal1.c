4dab999b7eb7afe82b3f7bd544127439365f8185
Jouvann <jouvan294@gmail.com>
2026-04-14 08:30:37 +0700

    Implement traffic light color output based on input

--- soal1.c
+++ soal1.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(void) {
+bool cekDigit(int N){
+    int temp, sum = 0;
+    if (N > 10){
+        while (N >= 1){
+            temp = N % 10;
+            sum += temp;
+            N /= 10;
+        }
+        if (sum % 2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+int main(){
+    int N;
+    printf("Masukan Tingkatan Lampu Merah : ");
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++){
+        if (i % 3 == 0 && i % 2 == 0){
+            printf("BIRU\n");
+        }
+        else if (i % 3 == 0){
+            printf("MERAH\n");
+        }
+        else if (i % 2 == 0){
+            printf("KUNING\n");
+        }
+        else if (cekDigit(i)){
+            printf("HIJAU\n");
+        }
+        else {
+            printf("%d\n", i);
+        }
+    }
+
     return 0;
 }
